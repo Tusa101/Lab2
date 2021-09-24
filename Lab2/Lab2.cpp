39aafbfa59408a7acab61eb01977bf6ee3e4916b
tuskanchik <amtuska@gmail.com>
2021-09-24 13:13:33 +0300

    Decl and def of funcs

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,7 +1,36 @@
 ﻿#include <iostream>
 using namespace std;
+/*вывод матрицы*/
+void DisplayMatrix(int** matrix, int size);
+/*ввод матрицы*/
+int** InputMatrix(int** matrix, int size);
+/*подсчет следа квадратной матрицы*/
+int TraceCount(int** matrix, int size);
+
+
+
+
+/*Вариант 4*/
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+}
+
+
+
+void DisplayMatrix(int** matrix, int size)
+{
+    for(int i=0;i<size;i++)
+    { }
+}
+
+int** InputMatrix(int** matrix, int size)
+{
+
+}
+
+int TraceCount(int** matrix, int size)
+{
+
 }
 
